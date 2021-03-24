8380ba2d9ee5ea8033e97a97295c1d6dce62ae9e
VAISHNAVI252007 <77496213+VAISHNAVI252007@users.noreply.github.com>
2021-03-24 20:03:29 +0530

    Add files via upload

---
+++ Bird.js
@@ -0,0 +1,19 @@
+class Bird extends BaseClass {
+  constructor(x,y){
+    super(x,y,50,50);
+    this.image = loadImage("sprites/bird.png");
+    this.image2=loadImage("sprites/smoke.png");
+    this.trajectory=[];
+  }
+
+  display() {
+    super.display();
+    if(this.body.position<200){
+      var pos=[this.body.position.x,this.body.position.y];
+      this.trajectory.push(pos);
+    }
+    for(var i=0;i<this.trajectory.length;i++){
+      image(this.image2,this.trajectory[i][0],this.trajectory[i][1]);
+    }
+  }
+}
---
+++ Slingshot.js
@@ -0,0 +1,46 @@
+class SlingShot{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 10
+
+            
+        }
+        this.sling1=loadImage("sprites/sling1.png");
+        this.sling2=loadImage("sprites/sling2.png");
+        this.pointB = pointB
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+    }
+
+    fly(){
+        this.sling.bodyA = null;
+    }
+    attach(body){
+        this.sling.bodyA=body;
+    }
+
+    display(){
+        if(this.sling.bodyA){
+            var pointA = this.sling.bodyA.position;
+            var pointB = this.pointB;
+            strokeWeight(4);
+            
+            if(pointA.x<210){
+                line(pointA.x-20, pointA.y, pointB.x-15, pointB.y);
+                line(pointA.x-20, pointA.y, pointB.x+20, pointB.y);
+            }
+            else{
+                line(pointA.x+20, pointA.y, pointB.x-15, pointB.y);
+                line(pointA.x+20, pointA.y, pointB.x+20, pointB.y);
+            }
+            
+        }
+        image(this.sling1,200,30)
+        image(this.sling2,170,30)
+
+    }
+    
+}
